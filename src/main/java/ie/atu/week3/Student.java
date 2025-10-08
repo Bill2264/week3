4a4d479e9bed096e3e04176d073c7f425339292f
Billy Murphy <G00465240@atu.ie>
2025-10-08 16:35:49 +0100

    Create constructor

---
+++ src/main/java/ie/atu/week3/Student.java
@@ -0,0 +1,13 @@
+package ie.atu.week3;
+
+
+public class Student {
+    String Name, Email, Course;
+
+    public Student(String tname,String temail,String tcourse) {
+        Name = tname;
+        Email = temail;
+        Course = tcourse;
+    }
+
+}
--- src/main/java/ie/atu/week3/StudentApp.java
+++ src/main/java/ie/atu/week3/StudentApp.java
@@ -1,4 +1,13 @@
 package ie.atu.week3;
 
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class StudentApp {
+    static void main() {
+        ArrayList<Student> students = new ArrayList<>();
+        Scanner scan1 = new Scanner(System.in);
+
+    }
 }
