62d59cb25b74cf241a904d24ec3d21a9ec694442
Billy Murphy <G00465240@atu.ie>
2025-10-08 16:35:49 +0100

    Made start ui

---
+++ src/main/java/ie/atu/week3/Student.java
@@ -0,0 +1,19 @@
+package ie.atu.week3;
+
+
+public class Student {
+    String Name, Email, Course;
+
+    public  Student()
+    {
+        Name = "";
+        Email = "";
+        Course = "";
+    }
+    public Student(String tname,String temail,String tcourse) {
+        Name = tname;
+        Email = temail;
+        Course = tcourse;
+    }
+
+}
--- src/main/java/ie/atu/week3/StudentApp.java
+++ src/main/java/ie/atu/week3/StudentApp.java
@@ -1,4 +1,25 @@
 package ie.atu.week3;
 
+import java.util.List;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+
 public class StudentApp {
+    static void main(String [] args) {
+        int count = 0;
+
+        List<Student> students = new ArrayList<>();
+        Scanner scan1 = new Scanner(System.in);
+
+        Student student = new Student();
+
+        System.out.println("Please enter the number of students: ");
+        int totalStudents = scan1.nextInt();
+
+        while(count<totalStudents)
+        {
+
+        }
+    }
 }
